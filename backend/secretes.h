e48cc5d5681149434a78c786c18bc940602deff8
Vedant Shinora <shinoravedant07@gmail.com>
2026-04-16 15:48:08 +0530

    newIP

--- backend/secretes.h
+++ backend/secretes.h
@@ -3,6 +3,6 @@
 
 #define WIFI_SSID       "Vedant_2G"
 #define WIFI_PASSWORD   "Ved12345"
-#define SERVER_URL "http://10.185.182.38:5000/api/sensor-data"
+#define SERVER_URL "http://10.228.36.38:5000/api/sensor-data"
 
 #endif
